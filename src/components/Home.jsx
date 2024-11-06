2ee1360de66f1cf6379ba617b1e4809179fc5ad5
mhdix <domanloom@gmail.com>
2024-11-06 04:25:57 -0800

    merge

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,6 @@ import NoteLists from "./NoteLIsts/NoteLists";
 
 const Home = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <div>navbar</div>
-      <div>noteList</div>
-      <div>
-        addNotes
-=======
     <div className="grid grid-cols-7">
       {/* navbar */}
       <div className="col-span-full">
@@ -20,7 +13,6 @@ const Home = () => {
       {/* addNote */}
       <div className="flex col-span-3 justify-around">
         <div>addNotes</div>
->>>>>>> 7aea97d41cda85f542b332378c80874d5f4118ed
         <div>filterNotes</div>
       </div>
 
